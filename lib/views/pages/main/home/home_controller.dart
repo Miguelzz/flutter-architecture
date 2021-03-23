79fda83d5c1a21c127a0e87b8a6264fe2ec36729
miguel angel ochoa torres <miguelangelochoatorres@gmail.com>
2021-03-22 22:54:34 -0500

    token home

--- lib/views/pages/main/home/home_controller.dart
+++ lib/views/pages/main/home/home_controller.dart
@@ -6,9 +6,13 @@ class HomeController extends GetxController {
   static final Services _services = Services.instance;
   static final AppDatabase _cache = AppDatabase.instance;
 
+  String? token;
+
   @override
   void onInit() async {
     super.onInit();
+    this.token = await _cache.getToken() ?? 'NO TOKEN';
+    update();
     print('HOME');
   }
 }
--- lib/views/pages/main/home/home_page.dart
+++ lib/views/pages/main/home/home_page.dart
@@ -5,7 +5,7 @@ import 'package:group/views/pages/main/home/home_controller.dart';
 import 'package:group/views/routes/routes_controller.dart';
 
 class HomePage extends StatelessWidget {
-  final controller = Get.put(HomeController());
+  final home = Get.put(HomeController());
   final MainController mainController = Get.find();
   final RouteController routeController = Get.find();
 
@@ -21,6 +21,12 @@ class HomePage extends StatelessWidget {
               height: size.height * 0.3,
               child: Center(
                 child: Text('txt_home'.tr),
+              ),
+            ),
+            Container(
+              width: size.width * 0.7,
+              child: GetBuilder<HomeController>(
+                builder: (_) => Text('${home?.token}'),
               ),
             ),
             Row(
